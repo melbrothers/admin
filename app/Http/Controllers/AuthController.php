73cd63b3b542caf2be15cf028e71d6cbecf1e25c
Lixing <zlxjackie@hotmail.com>
2018-12-28 21:02:27 +1100

    Fix app key

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required|alpha'
         ]);
 
