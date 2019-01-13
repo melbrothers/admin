245d8dd9bd0a259842b9579418cb3de3981864f7
Lixing Zhang <zlxjackie@hotmail.com>
2019-01-13 01:10:12 +0000

    Add comment enpoints

---
+++ app/Http/Resources/Comment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lixing
+ * Date: 2019-01-13
+ * Time: 11:43
+ */
+
+namespace App\Http\Resources;
+
+
+class Comment
+{
+
+}
---
+++ app/Http/Resources/CommentCollection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lixing
+ * Date: 2019-01-13
+ * Time: 12:06
+ */
+
+namespace App\Http\Resources;
+
+
+class CommentCollection
+{
+
+}
