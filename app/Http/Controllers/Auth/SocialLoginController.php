1628f56a2b12d635df3626083f1786d4255df30f
Lixing Zhang <zlxjackie@hotmail.com>
2019-02-25 11:31:07 +1100

    Hotfix: social login

--- app/Http/Controllers/Auth/SocialLoginController.php
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 
 use App\Http\Controllers\Controller;
-use App\LinkedSocialAccount;
+use App\Models\LinkedSocialAccount;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
